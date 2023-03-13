5bc2c6981a31c7d402144b9bbe0436fc3065e3eb
yuki-maeda-knowable <yuki_maeda@knowable.jp>
2023-03-13 19:17:41 +0900

    コメント追加と削除

---
+++ pages/api/comments/[commentId].js
@@ -0,0 +1,22 @@
+import { comments } from "@/data/comments";
+
+export default function handler(req, res) {
+  const {commentId} = req.query //req.queryに{ commentId: '1' }が入ってるから値だけ取得
+
+  if(req.method === 'GET'){
+    const comment = comments.find((comment) => comment.id === parseInt(commentId))
+    res.status(200).json(comment)  
+  }else if (req.method === 'DELETE'){
+    const deleteComment = comments.find((comment) => comment.id === parseInt(commentId))
+
+    // 配列から削除
+    const index = comments.findIndex((comment) => comment.id === parseInt(commentId))
+    comments.splice(index, 1)
+    res.status(200).json(deleteComment)
+  }else if (req.method === 'PUT'){
+    const editComment = comments.find((comment) => comment.id === parseInt(commentId))
+    alert('aaaaa')
+
+  }
+  
+}
